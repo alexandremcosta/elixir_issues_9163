16fc300c74118830b999ecf7795ffd6561b86d05
Alexandre Costa <alexandremcost@gmail.com>
2019-08-07 14:58:22 -0700

    replicate error

--- mix.exs
+++ mix.exs
@@ -21,6 +21,9 @@ defmodule ElixirIssues9163.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:remote_ip, "~> 0.1"},
+      {:timex, "~> 3.1"}
+
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
