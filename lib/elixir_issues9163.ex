d5359de9a475079bc88ecbcb1756a9eb473b3cf7
Alexandre Costa <alexandremcost@gmail.com>
2019-08-07 14:57:55 -0700

    initial commit: mix new

---
+++ lib/elixir_issues9163.ex
@@ -0,0 +1,18 @@
+defmodule ElixirIssues9163 do
+  @moduledoc """
+  Documentation for ElixirIssues9163.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ElixirIssues9163.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/elixir_issues9163_test.exs
@@ -0,0 +1,8 @@
+defmodule ElixirIssues9163Test do
+  use ExUnit.Case
+  doctest ElixirIssues9163
+
+  test "greets the world" do
+    assert ElixirIssues9163.hello() == :world
+  end
+end
